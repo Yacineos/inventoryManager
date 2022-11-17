4f2b037395ce6591a2dc3b55a025a668482c04b4
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-11-17 22:06:40 +0100

    first contact with angular

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MonPremierComponentComponent } from './mon-premier-component/mon-premier-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonPremierComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-app/src/app/mon-premier-component/mon-premier-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mon-premier-component',
+  templateUrl: './mon-premier-component.component.html',
+  styleUrls: ['./mon-premier-component.component.css']
+})
+export class MonPremierComponentComponent {
+
+}
