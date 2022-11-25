b9689b1464930cc8fa1b3a3337c6e94bf9800a8a
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-11-25 08:25:10 +0100

    learned 2 new notions
    I learned input data from parent component and sending data to it , and initiated to routing

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([{path:'',component:ProductListComponent},{path:'products/:productId',component:ProductDetailsComponent}])
   ],
   providers: [],
   bootstrap: [AppComponent]
