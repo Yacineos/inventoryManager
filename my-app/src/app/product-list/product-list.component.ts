7e811755076a4bc604f26a7ae18a2df0ea0c0328
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-11-26 08:02:14 +0100

    I fixed repeated 2 times component

--- my-app/src/app/product-list/product-list.component.ts
+++ my-app/src/app/product-list/product-list.component.ts
@@ -7,11 +7,15 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
+  temp = true ;
   products = products;
   share() {
     window.alert('The product has been shared');
   }
   onNotify() {
     window.alert('You will be notified when the product goes on sale');
+  }
+  detail(){
+    this.temp = false ;
   }
 }
