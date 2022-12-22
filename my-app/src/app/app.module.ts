1995bb6172fd6618b805f00cb743d38644c3f1a0
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-12-22 10:47:06 +0100

    creation of the authentification component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,25 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from "./auth/login/login.component";
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
-import { LoginComponent } from './auth/login/login.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AuthComponent,
-    SignUpComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AuthComponent,
+        LoginComponent,
+        SignUpComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        
+    ]
 })
 export class AppModule { }
