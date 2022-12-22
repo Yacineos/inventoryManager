8da622363667b37dec0673f94d9fef937d34e3f3
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-12-22 22:10:21 +0100

    more work in the login , and a problem show/hide

--- my-app/src/app/auth/login/login.component.ts
+++ my-app/src/app/auth/login/login.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
+  public visiblePassword() {
+    const passwordField = document.getElementById(
+      'password'
+    ) as HTMLInputElement;
+    const toggleButton = document.getElementById('toggle-password-visibility');
+    if(toggleButton != null)
+    toggleButton.addEventListener('click', () => {
+      if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+      } else {
+        passwordField.type = 'password';
+      }
+    });
+  }
 }
