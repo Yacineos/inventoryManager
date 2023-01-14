6daab52259aa62a821265258192e796b34eab43e
Yacineos <96418831+Yacineos@users.noreply.github.com>
2023-01-14 23:48:31 +0100

    fixed the lost css main login with tailwind

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -6,11 +6,14 @@ module.exports = {
   './src/app/app.component.html',
   './src/app/customers/customers.component.html', 
   './src/app/inventory/inventory.component.html', 
+  './src/app/auth/login/login.component.html', 
+  './src/app/auth/sign-up/sign-up.component.html', 
   './src/app/settings/settings.component.html',], 
   theme: {
     extend: {
       colors:{
         'mainpurple':'#5570f1',
+        'mainyellow':'#FFCC91',
         'mainwhite': '#ffffff',
         'mainbg': '#EEF0FA',
         'maingrey': '#8B8D97',
@@ -22,6 +25,10 @@ module.exports = {
         '30/100': '30%', 
         '32/100': '32%', 
         '66/100': '66%', 
+      },
+      fontFamily: {
+        'mainFont': ['Poppins', 'sans-serif'],
+        'secondaryFont': ['Inter', 'sans-serif'],
       }, 
     },
   },
